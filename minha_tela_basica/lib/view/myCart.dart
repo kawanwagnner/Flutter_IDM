9464a345028a2309447173587d2d228e58d2442a
Kawan Wagnner <kawanwagnner7@gmail.com>
2024-08-21 16:42:09 -0300

    new imgs

--- minha_tela_basica/lib/view/myCart.dart
+++ minha_tela_basica/lib/view/myCart.dart
@@ -6,22 +6,22 @@ class MyCartScreen extends StatelessWidget {
         name: 'Bell Pepper Red',
         quantity: '1kg, price',
         price: 4.99,
-        image: 'assets/img/banana.png'),
+        image: 'assets/img/image.png'),
     CartItem(
         name: 'Egg Chicken Red',
         quantity: '4pcs, price',
         price: 1.99,
-        image: 'assets/img/banana.png'),
+        image: 'assets/img/image.png'),
     CartItem(
         name: 'Organic Bananas',
         quantity: '12kg, price',
         price: 3.00,
-        image: 'assets/img/banana.png'),
+        image: 'assets/img/image.png'),
     CartItem(
         name: 'Ginger',
         quantity: '250gm, price',
         price: 2.99,
-        image: 'assets/img/banana.png'),
+        image: 'assets/img/image.png'),
   ];
 
   @override
@@ -29,7 +29,7 @@ class MyCartScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('My Cart'),
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.white,
         centerTitle: true,
         elevation: 0,
       ),
@@ -71,74 +71,81 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Padding(
-        padding: EdgeInsets.all(18),
-        child: Row(
-          children: [
-            Image.asset(item.image, width: 80, height: 80),
-            SizedBox(width: 16),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      color: Colors.white,
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        color: Colors.white,
+        child: Container(
+          child: Padding(
+            padding: EdgeInsets.all(18),
+            child: Row(
               children: [
-                Text(item.name,
-                    style:
-                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                SizedBox(height: 8),
-                Text(item.quantity,
-                    style: TextStyle(fontSize: 14, color: Colors.grey)),
-                Row(
+                Image.asset(item.image, width: 80, height: 80),
+                SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    IconButton(
-                      icon: Icon(
-                        Icons.remove_circle_outline,
-                        size: 45,
-                      ),
-                      onPressed: () {},
-                    ),
-                    Text('1', style: TextStyle(fontSize: 16)),
-                    IconButton(
-                      icon: Icon(
-                        Icons.add_circle_outline,
-                        size: 45,
-                      ),
-                      onPressed: () {},
+                    Text(item.name,
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold)),
+                    SizedBox(height: 8),
+                    Text(item.quantity,
+                        style: TextStyle(fontSize: 14, color: Colors.grey)),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            Icons.remove_circle_outline,
+                            size: 45,
+                          ),
+                          onPressed: () {},
+                        ),
+                        Text('1', style: TextStyle(fontSize: 16)),
+                        IconButton(
+                          icon: Icon(
+                            Icons.add_circle_outline,
+                            size: 45,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ],
                     ),
                   ],
                 ),
+                Spacer(),
+                SizedBox(width: 16),
+                Container(
+                  width: 80,
+                  height: 100,
+                  child: Stack(
+                    children: [
+                      // O texto que ficará visível normalmente
+                      Positioned(
+                        top: 0, // Ajuste a posição do ícone conforme necessário
+                        right:
+                            0, // Ajuste a posição do ícone conforme necessário
+                        child: IconButton(
+                          icon: Icon(Icons.close, size: 25, color: Colors.grey),
+                          onPressed: () {},
+                        ),
+                        // O ícone que será posicionado no canto superior
+                      ),
+                      Positioned(
+                        bottom: 10,
+                        right: 5,
+                        child: Text(
+                          '\$${item.price.toStringAsFixed(2)}',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
-            Spacer(),
-            SizedBox(width: 16),
-            Container(
-              width: 80,
-              height: 100,
-              child: Stack(
-                children: [
-                  // O texto que ficará visível normalmente
-                  Positioned(
-                    bottom: 10,
-                    right: 5,
-                    child: Text(
-                      '\$${item.price.toStringAsFixed(2)}',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  // O ícone que será posicionado no canto superior
-                  Positioned(
-                    top: 0, // Ajuste a posição do ícone conforme necessário
-                    right: 0, // Ajuste a posição do ícone conforme necessário
-                    child: IconButton(
-                      icon: Icon(Icons.close, size: 25, color: Colors.grey),
-                      onPressed: () {},
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
@@ -154,45 +161,61 @@ class CheckoutButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(30.0),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(10),
-        onTap: () {
-          // Ação ao clicar no botão
-        },
-        child: Container(
-            padding:
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 10.0),
-            decoration: BoxDecoration(
-              color: Colors.green,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Container(
-              width: 500,
-              height: 30,
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: 2,
-                    left: 150,
-                    child: const Text(
-                      'Go to Checkout',
-                      style: TextStyle(fontSize: 18, color: Colors.white),
-                    ),
-                  ),
-                  Positioned(
-                    right: 20,
-                    top: 5,
-                    child: Text(
-                      '\$${total.toStringAsFixed(2)}',
-                      style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                          backgroundColor: Colors.green[900]),
-                    ),
-                  )
-                ],
+      child: Container(
+        child: InkWell(
+          borderRadius: BorderRadius.circular(10),
+          onTap: () {
+            // Ação ao clicar no botão
+          },
+          child: Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 10.0),
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(15),
               ),
-            )),
+              child: Container(
+                width: 380,
+                height: 40,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 7,
+                      left: 120,
+                      child: const Text(
+                        'Go to Checkout',
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                    ),
+                    Positioned(
+                      right: 20,
+                      top: 0,
+                      child: Container(
+                        width: 60,
+                        height: 30,
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: Colors.green[700],
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Text(
+                          '\$${total.toStringAsFixed(2)}',
+
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.white,
+                          ),
+
+                          textAlign: TextAlign.center,
+
+                          // Aligns the text to the center
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )),
+        ),
       ),
     );
   }
